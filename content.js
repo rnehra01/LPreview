e297f62b6526dbabbc55e91e879bb47e7025b50c
rnehra01 <hnehra1@gmail.com>
2016-08-17 12:57:49 +0530

    Added Delay for opening the preview in content.js

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+var urlList;
+var _timer;
+var myWin;
+
+
 var urlList = document.getElementsByTagName("A");
 for (var i=0; i<urlList.length; i++){
     urlList[i].onmouseover=openPreview;
@@ -10,11 +15,14 @@ function openPreview() {
     //console.log(rect.right,rect.bottom);
     var width = 600;
     var height = width / 1.6;
-     
-    myWin=window.open(url1, "_blank", "resizable=yes, scrollbars=yes, titlebar=0, addressbar=0, menubar=0, toolbar=0, width=" + width + ", height=" + height +
-        "");
+    //open window after waiting for 2 sec 
+    _timer=setTimeout(function(){
+        myWin=window.open(url1, "_blank", "resizable=yes, scrollbars=yes, titlebar=0, addressbar=0, menubar=0, toolbar=0, width=" + width + ", height=" + height +
+        "");                            
     myWin.moveBy(rect.right,rect.bottom);
     myWin.focus();
+    },2000);
+    
 }
 
 function closePreview(){
