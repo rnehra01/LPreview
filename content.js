c416f907ee33658fd479d42bac50c4af1797ccf5
Ravinder Nehra <hnehra1@gmail.com>
2016-08-18 13:42:06 +0530

    Improves the positioning of opening preview

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 var urlList;
 var _timer;
 var myWin;
+var mouseX=0,mouseY=0;
 
 
 urlList = document.getElementsByTagName("A");
@@ -16,11 +17,11 @@ function openPreview() {
     var width = 600;
     var height = width / 1.6;
     //open window after waiting for 2 sec 
+    mouseX = window.event.screenX;
+    mouseY = window.event.screenY;
     _timer=setTimeout(function(){
-        myWin=window.open(url1, "_blank", "resizable=yes, scrollbars=yes, titlebar=0, addressbar=0, menubar=0, toolbar=0, width=" + width + ", height=" + height +
-        "");                            
-    myWin.moveBy(rect.right,rect.bottom);
-    myWin.focus();
+        myWin=window.open(url1, "_blank", 'resizable=yes, scrollbars=yes, titlebar=0, addressbar=0, menubar=0, toolbar=0,top=' + mouseY + ',screenY=' + mouseY + ',left=' + mouseX + ',screenX=' + mouseX + ',width=' + width + ',height=' + height);
+        if (myWin.opener == null) myWin.opener = self;
     },2000);
     
 }
@@ -29,3 +30,6 @@ function closePreview(){
     myWin.close();
     clearTimeout(_timer);
 }
+
+
+
