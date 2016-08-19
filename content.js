f3e33d09a9eb2b38d07df974225372f57fbc195d
rnehra01 <hnehra1@gmail.com>
2016-08-19 16:41:27 +0530

    Added a small button

--- content.js
+++ content.js
@@ -19,16 +19,29 @@ function openPreview() {
     //open window after waiting for 2 sec 
     mouseX = window.event.screenX;
     mouseY = window.event.screenY;
+
     _timer=setTimeout(function(){
         myWin=window.open(url1, "_blank", 'resizable=yes, scrollbars=yes, titlebar=0, addressbar=0, menubar=0, toolbar=0,top=' + mouseY + ',screenY=' + mouseY + ',left=' + mouseX + ',screenX=' + mouseX + ',width=' + width + ',height=' + height);
         if (myWin.opener == null) myWin.opener = self;
     },2000);
-    
+
+    var pos=getOffset(this);
+    var button=document.createElement("BUTTON");
+    button.setAttribute("style","position:absolute;heigth:20px;width:50px;z-index:2000;top:"+pos.top+"px;left:"+pos.left+"px");
+    document.body.appendChild(button);
 }
 
 function closePreview(){
-    myWin.close();
     clearTimeout(_timer);
+    myWin.close();
+}
+
+function getOffset(el) {
+  el = el.getBoundingClientRect();
+  return {
+    left: el.left + window.scrollX+el.width,
+    top: el.top + window.scrollY+el.height
+  }
 }
 
 
